de82a98e7d6ea901423dcd0cc96e979a7be6f1e8
igorbirkan <igor.birkan@gmail.com>
2018-06-18 16:33:02 +0300

    v2

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,38 @@
-this.onload=function(){
-    var divCount = document.getElementsByClassName("center-div").length;
-    for(var i=0;i<divCount;i++){
-        document.getElementsByClassName("center-div")[i].addEventListener("click", function(){        
-            this.style.backgroundColor = getRandomColor();
-            this.lastUpdate();
-        });   
 
-        document.getElementsByClassName("center-div")[i].originText = document.getElementsByClassName("center-div")[i].innerHTML;
-        
-        document.getElementsByClassName("center-div")[i].lastUpdate = function(){
-            var today = new Date();
-            var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-            var dateTime = date+' '+time;
-            this.innerHTML=this.originText+" : " + dateTime;
-        }
-    }
+document.onclick=function(event){   
+    var clickedObj = event.target;
 
-    var specialCount = document.getElementsByClassName("special").length;
-    for(var j=0;j<specialCount;j++){
-        document.getElementsByClassName("special")[j].addEventListener("click", function(){        
-            this.style.borderWidth="5px";
-            this.style.borderColor = getRandomColor();
-        });        
+    switch ( true ){
+        case /center-div/.test(clickedObj.className) :
+            if(typeof(clickedObj.lastUpdate) =="undefined"){
+                clickedObj.originText = event.target.innerHTML;
+                clickedObj.lastUpdate = setLastUpdate;
+            }
+
+            clickedObj.style.backgroundColor = getRandomColor();
+            clickedObj.lastUpdate(clickedObj);  
+            
+            if(clickedObj.className.includes("special")){
+                clickedObj.style.borderWidth="5px";
+                clickedObj.style.borderColor = getRandomColor();  
+            }
+            break;
+        case /main-button/.test(clickedObj.className) :
+            var newDiv = document.createElement("div");
+            newDiv.className="center-div";
+            var textBox = document.getElementById("person-name");
+            var pesonName = textBox.value;
+            newDiv.innerHTML  = pesonName;
+
+            var mainDiv = document.getElementById("main-div");
+            mainDiv.appendChild(newDiv);
+
+            textBox.value="";  
+            break;
     }
 }
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
 
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
@@ -33,4 +41,12 @@ function getRandomColor() {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
+}
+
+function setLastUpdate(obj){
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    obj.innerHTML=obj.originText+" : " + dateTime;
 }
